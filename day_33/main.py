c14af6868c33c7ef98999684a3f68a064ddb5f97
Fernando Arias <fa01.arias@gmail.com>
2024-08-22 00:47:36 -0400

    Update and rename Main.py to main.py

--- day_33/main.py
+++ day_33/main.py
@@ -2,26 +2,35 @@ import requests
 from datetime import datetime
 import smtplib
 import time
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Data
-MY_LAT = 40.693320  # Your latitude
-MY_LONG = -73.905790  # Your longitude
+MY_LAT = 00.00  # Your latitude
+MY_LONG = 00.00  # Your longitude
 EMAIL = 'example01@example.com'  # placeholder
 RECIPIENT = 'example@example.com'  # placeholder
-PASSWORD = 'App Password Here'
+PASSWORD = 'App Password Here' # placeholder
 
 
 # checks if the ISS is close your location
 def is_iss_close():
-    response = requests.get(url="http://api.open-notify.org/iss-now.json")
-    response.raise_for_status()
-    data = response.json()
+    try:
+        response = requests.get(url=getenv("ISS_API_URL"))
+        response.raise_for_status()
+        data = response.json()
 
-    iss_latitude = float(data["iss_position"]["latitude"])
-    iss_longitude = float(data["iss_position"]["longitude"])
+        iss_latitude = float(data["iss_position"]["latitude"])
+        iss_longitude = float(data["iss_position"]["longitude"])
 
-    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
-        return True, iss_latitude, iss_longitude
+        if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+            return True, iss_latitude, iss_longitude
+        return False, None, None
+    except requests.RequestException as e:
+        print(f"Error fetching ISS data: {e}")
+        return False, None, None
 
 
 # collects data of your sunset and sunrise time in your area
@@ -33,7 +42,7 @@ def is_night():
     }
 
     # collecting the sunrise and sunset hours
-    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response = requests.get(getenv("SUNRISE_API_URL"), params=parameters)
     response.raise_for_status()
     data = response.json()
     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
@@ -44,6 +53,7 @@ def is_night():
 # will continue to check if its nighttime and the ISS is close to your location for every minute
 while True:
     # current time
+    print("test")
     time_now = datetime.now()
     is_close, iss_latitude, iss_longitude = is_iss_close()
     sunset, sunrise = is_night()
