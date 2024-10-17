c1a1ce8ca0838241b474ee34ffdf6fcd943aa08e
Francerfernance <thekidofsteel01@gmail.com>
2024-10-16 20:00:08 -0400

    Day 16

--- day_16/coffeeMachineV2.py
+++ day_16/coffeeMachineV2.py
@@ -1,29 +1,29 @@
-from menu import Menu, MenuItem
-from Main import CoffeeMaker
-from money_machine import MoneyMachine
+class CoffeeMaker:
+    """Models the machine that makes the coffee"""
+    def __init__(self):
+        self.resources = {
+            "water": 300,
+            "milk": 200,
+            "coffee": 100,
+        }
 
-money_machine = MoneyMachine()
-coffee_maker = CoffeeMaker()
-coffee_menu = Menu()
+    def report(self):
+        """Prints a report of all resources."""
+        print(f"Water: {self.resources['water']}ml")
+        print(f"Milk: {self.resources['milk']}ml")
+        print(f"Coffee: {self.resources['coffee']}g")
 
-is_on = True
+    def is_resource_sufficient(self, drink):
+        """Returns True when order can be made, False if ingredients are insufficient."""
+        can_make = True
+        for item in drink.ingredients:
+            if drink.ingredients[item] > self.resources[item]:
+                print(f"Sorry there is not enough {item}.")
+                can_make = False
+        return can_make
 
-while is_on:
-    menu = coffee_menu.get_items()
-    choice = input(f"WHAT WOULD YOU LIKE TO DRINK? {menu}\n").lower()
-
-    # secret command
-    if choice == "report":
-        coffee_maker.report()
-        money_machine.report()
-    elif choice == "off":
-        is_on = False
-    else:
-        coffee = coffee_menu.find_drink(choice)
-        if coffee_maker.is_resource_sufficient(coffee):
-            if money_machine.make_payment(coffee.cost):
-                coffee_maker.make_coffee(coffee)
-
-
-
-
+    def make_coffee(self, order):
+        """Deducts the required ingredients from the resources."""
+        for item in order.ingredients:
+            self.resources[item] -= order.ingredients[item]
+        print(f"Here is your {order.name} ☕️. Enjoy!")
